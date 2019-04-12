5898f1e2741ef7a36ddcfb8051624fddaf00fead
longbang <longbang@ifalo.com.tw>
2019-04-12 10:13:57 +0800

    測試

--- database/migrations/2019_04_11_143258_create_user_login_table.php
+++ database/migrations/2019_04_11_143258_create_user_login_table.php
@@ -17,16 +17,15 @@ class CreateUserLoginTable extends Migration
         Schema::create('user_login', function (Blueprint $table) {
             $table->increments('id')->comment('PK');
             $table->smallInteger('user_id')->unsigned()->comment('使用者ID');
-
-            $table->foreign('user_id', 'user_login_ibfk_1')
-                ->references('id')->on('users')
-                ->onUpdate('cascade')->onDelete('cascade');
-
             $table->string('user_account', 12)->default('')->comment('使用者帳號');
             $table->string('user_password', 10)->default('')->comment('使用者密碼');
             $table->string('login_ip',46)->comment('登入IP(強制登出，則塞入空值)');
             $table->unsignedTinyInteger('status')->default(1)->comment('狀態(1:登入成功、2:正常登出、3:強制登出)');
             $table->timestamps();
+
+            $table->foreign('user_id', 'user_login_ibfk_1')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
         });
         DB::statement("ALTER TABLE `user_login` COMMENT '使用者登入日誌'");
     }
